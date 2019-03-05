28af66f915db23a2ab1df08647cfe553d763bdba
phatompong <basukaman123@gmail.com>
2019-03-05 17:37:37 +0700

    edit title

--- admin_area/includes/header.php
+++ admin_area/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>M-Dev Store Admin Area</title>
+    <title>OTOP : แผงควบคุมระบบหลังร้าน </title>
     <link rel="stylesheet" href="css/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
